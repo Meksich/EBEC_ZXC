5170de806b06e9594c09e4b44627081937b6ac87
BogdanDidukh2003 <diduhbogdan441@gmail.com>
2021-04-10 14:04:26 +0300

    added functions for web remote control

--- algos/algos.ino
+++ algos/algos.ino
@@ -1,6 +1,7 @@
 #include <math.h>
 #include <Servo.h> 
 #include <Stepper.h>
+#define _USE_MATH_DEFINES
 
 
 Servo myservo;
@@ -8,63 +9,104 @@ Stepper leftStepper = Stepper(1, 1, 2, 3, 4);
 Stepper rightStepper = Stepper(1, 5, 6, 7, 8);
 float ASIXRAD = 0.2;  //in meters
 float STEP = 0.407; //in meters
+int currrentDegree = 90;
+
+bool moveForward = false;
+bool moveBackwards = false;
+bool turnRight = false;
+bool turnLeft = false;
+
+enum TurnPositions {LEFT = 30, CENTER = 90, RIGHT = 150};
 
 struct Cord
 {
-  float cordX
-  float cordY  
-}
+  float cordX;
+  float cordY;
+};
 
 float getDistanceInSteps(Cord start, Cord finish)
 {
-  return sqrt(pow((start.cordX - finish.cordX),2) + pow((start.cordY - finish.cordY),2))) / STEP;
-}
+  return sqrt(pow((start.cordX - finish.cordX),2) + pow((start.cordY - finish.cordY),2)) / STEP;
+};
 
 float getAngleRad(Cord start, Cord finish)
 {
   return atan((finish.cordY - start.cordY) / (finish.cordX - start.cordX));
-}
+};
 
-void rotateStill(float angle)
+void move(float distanceLength)
 {
-  float arcLengthSteps = (angle * ASIXRAD) / STEP;
-  if (angle <= 3.14)
+  float distanceLengthSteps = distanceLength / STEP;
+  for (float i = 0; i < distanceLengthSteps; i += 0.01)
+    {
+      leftStepper.step(0.01);
+      rightStepper.step(0.01);
+      digitalWrite(12, LOW);
+      
+    };
+};
+
+void rotateStill(float angleRad)
+{
+  float arcLengthSteps = (angleRad * ASIXRAD) / STEP;
+  if (angleRad <= M_PI)
   {
-    leftStepper.step(arcLengthSteps);
-    rightStepper.step(-arcLengthSteps);
+    for (int i = 0; i < arcLengthSteps; i += 0.01)
+    {
+      leftStepper.step(0.01);
+      rightStepper.step(-0.01);
+    };
   }
   else  
   {
-    leftStepper.step(-arcLengthSteps);
-    rightStepper.step(arcLengthSteps);
+    for (int i = 0; i < arcLengthSteps; i += 0.01)
+    {
+      leftStepper.step(-0.01);
+      rightStepper.step(0.01);
+    };
   };
-}
-  
+};
 
 void doMision(Cord start, Cord finish)
 {
-  rotateStill(getAngleRad(start, finish))
-  dellay(1000)
+  rotateStill(getAngleRad(start, finish));
+  delay(1000);
   leftStepper.step(getDistanceInSteps(start, finish));
   rightStepper.step(getDistanceInSteps(start, finish));
-}
+};
+
+void rotateFrontAsix(int angleGrad)
+{
+  myservo.write(90 + angleGrad);
+};
 
 void setup() {
   // put your setup code here, to run once:
   myservo.attach(9);
   myservo.write(90);
-  myStepper.setSpeed(100);
-}
+  leftStepper.setSpeed(100);
+  rightStepper.setSpeed(100);
+};
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int xMyCord = 12;
-  int yMyCord = 15;
-  int xObjCord = 20;
-  int yObjCord = 30;
-  int xDifCord = xMyCord - xObjCord;
-  int yDifCord = yMyCord - yObjCord;
-  int degree = atan(yDifCord / xDifCord)
-  
-  
-}
+  int checkedPosition = TurnPositions::LEFT;
+  int checkedDegree = static_cast < int > (checkedPosition);
+  if(moveForward)
+  {
+    move(0.01);
+  };
+  if(moveBackwards)
+  {
+    move(-0.01);
+  };
+  if(checkedDegree > currrentDegree)
+  {
+    currrentDegree ++;
+    myservo.write(currrentDegree);
+  };
+  if(checkedDegree < currrentDegree)
+  {
+    currrentDegree --;
+    myservo.write(currrentDegree);
+  }; 
+};
